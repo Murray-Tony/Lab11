d8a20e82db9d9fc4b22d3a3405d864eb2b1b8ed0
Murray-Tony <85173110+Murray-Tony@users.noreply.github.com>
2021-11-10 19:29:37 +0000

    Initial commit

---
+++ Student.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Student{
+  public string Id{get;set;}
+  public string Title{get;set;}
+  public DateTime DateOfEmployment{get;set;}
+  public double YearlySalary{get;set;}
+  public bool Tenured{get;set;}
+  public void SetInstructor()
+  {
+    
+  }
+
+}
