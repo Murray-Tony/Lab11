af89e24d49645dfc252c378bf43abded185c4d89
Tony Murray <85173110+Murray-Tony@users.noreply.github.com>
2021-11-12 01:43:51 +0000

    c

--- main.cs
+++ main.cs
@@ -6,15 +6,14 @@ class Program {
     Faculty f1 = new Faculty("Tony", "Murray");
     f1.School = "Tri-C";
     f1.DateOfEmployment = Convert.ToDateTime("11/11/2000");
+    f1.Intro();
     Console.WriteLine("Tenure status: "+ f1.Tenured);
     Console.WriteLine("I will now attempt to grant Tenure status.");
     f1.GrantTenure();
     Console.WriteLine("New Tenure status: "+ f1.Tenured);
-    f1.Intro();
+    Console.WriteLine("How about some promotions?");
     f1.Promote();
-    f1.Intro();
     f1.Promote();
-    f1.Intro();
     f1.Promote();
     f1.Intro();
     Console.WriteLine("New faculty:");
@@ -26,5 +25,9 @@ class Program {
     Console.WriteLine("I will now attempt to grant Tenure status.");
     f2.GrantTenure();
     Console.WriteLine("New Tenure status: "+ f2.Tenured);
+    Console.WriteLine("How about some promotions?");
+    f2.Promote();
+    f2.Promote();
+    f2.Intro();
   }
 }
